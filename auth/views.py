1d64e8806f16fac521673db91c2eeb48a7d7c7ec
Mamba1099 <mambakaranja240@gmail.com>
2024-06-08 14:22:36 +0300

    register urls patter and views for user model serializers

--- auth/views.py
+++ auth/views.py
@@ -1,12 +1,15 @@
 """urls for auth app."""
+
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
 class RegisterView(generics.CreateAPIView):
     """view for user registration."""
+
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
