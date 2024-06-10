4e7061b368c28b2594211887443b9d50467051ba
Mamba1099 <mambakaranja240@gmail.com>
2024-06-10 23:57:18 +0300

    register models to site

---
+++ animal/admin.py
@@ -0,0 +1,17 @@
+"""register models to site."""
+from django.contrib import admin
+from .models import (
+    Animal,
+    Purchased,
+    LocallyServiced,
+    AIPredeterminedServiced,
+    AInotPredeterminedServiced
+)
+
+admin.site.register(Animal)
+admin.site.register(Purchased)
+admin.site.register(LocallyServiced)
+admin.site.register(AIPredeterminedServiced)
+admin.site.register(AInotPredeterminedServiced)
+
+
