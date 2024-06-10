f0b214070e5cc18501be6daab351cb5a47d09990
Mamba1099 <mambakaranja240@gmail.com>
2024-06-10 23:55:45 +0300

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 
