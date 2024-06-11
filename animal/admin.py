b9b264ec62b23d03519a5aa1393c084f44ecfe4d
Mamba1099 <mambakaranja240@gmail.com>
2024-06-12 00:32:11 +0300

    feat: register treatment model to admin site

--- animal/admin.py
+++ animal/admin.py
@@ -1,4 +1,5 @@
 """register models to site."""
+
 from django.contrib import admin
 from .models import (
     Animal,
@@ -6,6 +7,9 @@ from .models import (
     LocallyServiced,
     AIPredeterminedServiced,
     AInotPredeterminedServiced,
+    TreatmentRecords,
+    MedicineTreatment,
+    Dosagetreatment
 )
 
 admin.site.register(Animal)
@@ -13,3 +17,6 @@ admin.site.register(Purchased)
 admin.site.register(LocallyServiced)
 admin.site.register(AIPredeterminedServiced)
 admin.site.register(AInotPredeterminedServiced)
+admin.site.register(TreatmentRecords)
+admin.site.register(MedicineTreatment)
+admin.site.register(Dosagetreatment)
