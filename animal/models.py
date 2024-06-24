e5163b70af9a7c8cd9a9b81511573545d1836e63
Mamba1099 <mambakaranja240@gmail.com>
2024-06-24 13:53:54 +0300

    feat: adjust models for animal fields with django built in validators

--- animal/models.py
+++ animal/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import (
+    MinLengthValidator,
+)
 
 
 class Animal(models.Model):
-
+    id = models.AutoField(primary_key=True, default=1)
     SOURCE_CHOICES = (
         ("purchased", "Purchased"),
         ("locally_serviced", "Locally Serviced"),
         ("ai_predetermined", "AI Predetermined"),
-        ("ai_not_predetermined", "AI not Predetermined"),
+        ("ai_non_predetermined", "AI not Predetermined"),
     )
 
-    name = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     image = models.ImageField(
         upload_to="static/", null=True, blank=True, default="static/default.jpg"
     )
-    breed = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     gender = models.CharField(max_length=1, choices=[("M", "male"), ("F", "female")])
     date_of_next_service = models.DateField(
         null=True, blank=True, help_text="Only applicable for female"
     )
-
     source = models.CharField(null=True, max_length=50, choices=SOURCE_CHOICES)
 
     def __str__(self):
@@ -28,10 +30,8 @@ class Animal(models.Model):
 
 
 class Purchased(models.Model):
-    """Model for purchased animals."""
-
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    seller_name = models.CharField(max_length=50)
+    seller_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     date_purchased = models.DateField(default=timezone.now)
 
     def __str__(self):
@@ -39,10 +39,8 @@ class Purchased(models.Model):
 
 
 class LocallyServiced(models.Model):
-    """Model for locally serviced animals."""
-
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    name_of_owner = models.CharField(max_length=50)
+    name_of_owner = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     date_of_service = models.DateField(default=timezone.now)
     birth_date = models.DateField(default=timezone.now)
 
@@ -50,7 +48,7 @@ class LocallyServiced(models.Model):
         return self.animal
 
 
-class AIPredeterminedServiced(models.Model):
+class AIPredeterminedServiceAnimal(models.Model):
     """Model for AI predetermined serviced animals."""
 
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
@@ -67,7 +65,7 @@ class AIPredeterminedServiced(models.Model):
         return self.animal
 
 
-class AInotPredeterminedServiced(models.Model):
+class AInonPredeterminedServiceAnimal(models.Model):
     """Model for AI not predetermined serviced animals."""
 
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
@@ -78,32 +76,37 @@ class AInotPredeterminedServiced(models.Model):
         return self.animal
 
 
-class TreatmentRecords(models.Model):
-    """models for treatment of each animal"""
-
+class MedicineTreatment(models.Model):
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     date_of_diagnosis = models.DateField(default=timezone.now)
+    name_of_vet = models.CharField(
+        max_length=50,
+        validators=[
+            MinLengthValidator(
+                2, message="Name of vet must be at least 2 characters long."
+            )
+        ],
+    )
+    date_of_medicine_treatment = models.DateField(default=timezone.now)
+    current_state = models.CharField(
+        max_length=1, choices=(("D", "dead"), ("C", "cured"))
+    )
+    cured = models.CharField(max_length=1, choices=(("S", "sold"), ("N", "not sold")))
 
     def __str__(self):
         return self.animal
-
-
-class MedicineTreatment(models.Model):
-    animal = models.ForeignKey(TreatmentRecords, on_delete=models.CASCADE)
-    name_of_vet = models.CharField(max_length=50)
-    date_of_medicine_treatment = models.DateField(default=timezone.now)
-    current_state = models.CharField(
-        max_length=1, choices=(("D", "dead"), ("C", "cured"))
-    )
-    cured = models.CharField(max_length=1, choices=(("S", "sold"), ("N", "not sold")))
-
-
 class Dosagetreatment(models.Model):
-    animal = models.ForeignKey(TreatmentRecords, on_delete=models.CASCADE)
-    name_of_vet = models.CharField(max_length=50)
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    date_of_diagnosis = models.DateField(default=timezone.now)
+    name_of_vet = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     date_of_medicine_treatment = models.DateField(default=timezone.now)
-    dosage_treatment_used = models.CharField(max_length=50)
+    dosage_treatment_used = models.CharField(
+        max_length=50, validators=[MinLengthValidator(2)]
+    )
     current_state = models.CharField(
         max_length=1, choices=(("D", "dead"), ("C", "cured"))
     )
     cured = models.CharField(max_length=1, choices=(("S", "sold"), ("N", "not sold")))
+
+    def __str__(self):
+        return self.animal
