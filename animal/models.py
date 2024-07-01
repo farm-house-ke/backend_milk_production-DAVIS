aab3d82d422f6ca8dab36ee3e0b2a4290ab89123
Mamba1099 <mambakaranja240@gmail.com>
2024-07-02 01:11:51 +0300

    set blank to false for important fields

--- animal/models.py
+++ animal/models.py
@@ -1,24 +1,25 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 from django.core.validators import (
     MinLengthValidator,
 )
 
 
 class PurchasedAnimal(models.Model):
-    animal_name = models.CharField(
-        null=True, blank=True, max_length=50, validators=[MinLengthValidator(2)]
-    )
+    animal_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     image = models.ImageField(
-        upload_to="static/", null=True, blank=True, default="static/default.jpg"
+        upload_to="static/", blank=False, default="static/default.jpg"
     )
     breed = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     gender = models.CharField(max_length=1, choices=[("M", "male"), ("F", "female")])
     date_of_next_service = models.DateField(
-        null=True, blank=True, help_text="Only applicable for female"
+        null=True,
+        blank=False,
+        help_text="Only applicable for female",
+        default=timezone.now(),
     )
     seller_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
-    date_purchased = models.DateField(default=datetime.date.today)
+    date_purchased = models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.animal_name}"
@@ -28,63 +29,53 @@ class LocallyServicedAnimal(models.Model):
     """Model for locally serviced animals"""
 
     animal_name = models.CharField(
-        null=True, blank=True, max_length=50, validators=[MinLengthValidator(2)]
+        blank=False, max_length=50, validators=[MinLengthValidator(2)]
     )
     image = models.ImageField(
-        upload_to="static/", null=True, blank=True, default="static/default.jpg"
+        upload_to="static/", blank=False, default="static/default.jpg"
     )
     breed = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     gender = models.CharField(max_length=1, choices=[("M", "male"), ("F", "female")])
     date_of_next_service = models.DateField(
-        null=True, blank=True, help_text="Only applicable for female"
+        null=True,
+        blank=False,
+        help_text="Only applicable for female",
+        default=timezone.now(),
     )
     name_of_owner = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
-    date_of_service = models.DateField(default=datetime.date.today)
-    birth_date = models.DateField(default=datetime.date.today)
+    date_of_service = models.DateField(default=timezone.now())
+    birth_date = models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.animal_name}"
 
 
-class AIPredeterminedServiceAnimal(models.Model):
+class AIPredeterminedAndNonPredeterminedServiceAnimal(models.Model):
     """Model for AI predetermined serviced animals."""
 
     animal_name = models.CharField(
-        null=True, blank=True, max_length=50, validators=[MinLengthValidator(2)]
+        blank=False, max_length=50, validators=[MinLengthValidator(2)]
     )
     image = models.ImageField(
-        upload_to="static/", null=True, blank=True, default="static/default.jpg"
+        upload_to="static/", blank=False, default="static/default.jpg"
     )
     breed = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     gender = models.CharField(
-        max_length=1, null=True, blank=True, choices=[("M", "male"),("F", "female")], default="female"
+        max_length=1,
+        blank=False,
+        choices=[("M", "male"), ("F", "female")],
+        default="female",
     )
     date_of_next_service = models.DateField(
-        null=True, blank=True, help_text="only applicable for female"
+        null=True,
+        blank=False,
+        help_text="only applicable for female",
+        default=timezone.now(),
     )
-    date_of_service = models.DateField(default=datetime.date.today)
-    birth_date = models.DateField(default=datetime.date.today)
-
-    def __str__(self):
-        return f"{self.animal_name}"
-
-
-class AInonPredeterminedServiceAnimal(models.Model):
-    """Model for AI not predetermined serviced animals."""
-
-    animal_name = models.CharField(
-        null=True, blank=True, max_length=50, validators=[MinLengthValidator(2)]
+    date_of_service = models.DateField(
+        default=timezone.now(),
     )
-    image = models.ImageField(
-        upload_to="static/", null=True, blank=True, default="static/default.jpg"
-    )
-    breed = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
-    gender = models.CharField(max_length=1, choices=[("M", "male"), ("F", "female")])
-    date_of_next_service = models.DateField(
-        null=True, blank=True, help_text="Only applicable for female"
-    )
-    date_of_service = models.DateField(default=datetime.date.today)
-    birth_date = models.DateField(default=datetime.date.today)
+    birth_date = models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.animal_name}"
