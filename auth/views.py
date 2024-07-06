96becb128a3554283b8c73370c32cd9ea40e5f15
Mamba1099 <mambakaranja240@gmail.com>
2024-07-06 23:04:38 +0300

    feat: create function to handle user registration using serializer

--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,8 @@
 """urls for auth app."""
 
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
-from rest_framework.permissions import AllowAny
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -11,12 +11,23 @@ class RegisterView(generics.CreateAPIView):
     """view for user registration."""
 
     queryset = User.objects.all()
-    permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {
+                "user": RegisterSerializer(
+                    user, context=self.get_serializer_context()
+                ).data
+            },
+            status=status.HTTP_201_CREATED
+        )
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
     """view for obtaining token pair."""
 
-    permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
