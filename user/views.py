a825e9c874ec3a918f277cf4d1f4153f5fc93433
Mamba1099 <mambakaranja240@gmail.com>
2024-08-01 21:17:32 +0300

    feat: create view for user sign up and login

--- user/views.py
+++ user/views.py
@@ -1,29 +1,37 @@
 """views for user model."""
 
-from django.contrib.auth import authenticate, get_user_model
-from rest_framework.views import APIView
-from rest_framework import generics
+from django.contrib.auth import login, get_user_model
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
-
-from .serializers import UserSignUpSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
 
 User = get_user_model()
-class UserLoginView(APIView):
-    """view for login user"""
-    def post(self, request):
-        """login user."""
-        username = request.data.get("username")
-        password = request.data.get("password")
-        
-        user = authenticate(username=username, password=password)
-        if user:
-            token = AccessToken.for_user(user)
-            return Response({"token": str(token)})
-        else:
-            return Response({"error": "wrong credentials"}, status=400)
+from .serializers import UserSignUpSerializer, UserLoginSerializer
 
-class UserList(generics.ListCreateAPIView):
-    """view for user list."""
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ViewSet for user model."""
+
     queryset = User.objects.all()
     serializer_class = UserSignUpSerializer
+
+    @action(detail=False, methods=["post"], serializer_class=UserLoginSerializer)
+    def login(self, request):
+        """Login user."""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        login(request, user)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response(
+            {
+                "token": token.key,
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                    "username": user.username,
+                },
+            },
+            status=status.HTTP_200_OK,
+        )
