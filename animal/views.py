16ed33afaefe044da70e5ba0b7b9cf4176b6ab93
Mamba1099 <mambakaranja240@gmail.com>
2024-08-15 22:42:00 +0300

    feat: create view for gender distribution pie chart

--- animal/views.py
+++ animal/views.py
@@ -1,4 +1,10 @@
 """views for serializer models."""
+from django.http import JsonResponse
+from django.db import models
+from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework.response import Response
+from rest_framework import status
 
 from rest_framework import viewsets
 from .serializers import (
@@ -53,3 +59,23 @@ class DosageTreatmentViewSet(viewsets.ModelViewSet):
 class AnimalBaseViewSet(viewsets.ModelViewSet):
     queryset = AnimalBase.objects.all()
     serializer_class = AnimalBaseSerializer
+
+
+class GenderDistributionView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        try:
+            gender_counts = AnimalBase.objects.values("gender").annotate(
+                count=Count("gender")
+            )
+            data = {"male": 0, "female": 0}
+            for entry in gender_counts:
+                if entry["gender"] == "M":
+                    data["male"] = entry["count"]
+                elif entry["gender"] == "F":
+                    data["female"] = entry["count"]
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
