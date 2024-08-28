e61b94c4626fefea45a6d70cb5a8ea26adf2598f
Mamba1099 <mambakaranja240@gmail.com>
2024-08-28 20:37:37 +0300

    feat: register animal models to site

--- animal/admin.py
+++ animal/admin.py
@@ -1,9 +1,19 @@
 """register models to site."""
 
 from django.contrib import admin
-from .models import PurchasedAnimal, LocallyServicedAnimal, AIPredeterminedServiceAnimal
+from .models import (
+    PurchasedAnimal,
+    LocallyServicedAnimal,
+    AIPredeterminedServiceAnimal,
+    AInonPredeterminedServicedAnimal,
+    MedicineTreatment,
+    DosageTreatment,
+)
 
 
 admin.site.register(PurchasedAnimal)
 admin.site.register(LocallyServicedAnimal)
 admin.site.register(AIPredeterminedServiceAnimal)
+admin.site.register(AInonPredeterminedServicedAnimal)
+admin.site.register(MedicineTreatment)
+admin.site.register(DosageTreatment)
