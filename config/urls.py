5af6428df8729cb2ec8a81ab2c6a15c5b338d6bc
Mamba1099 <mambakaranja240@gmail.com>
2024-08-28 20:40:21 +0300

    feat: register urls for user_api app

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("animal/", include("animal.urls")),
-    path("user/", include("user.urls")),
+    path("user/", include("user_api.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
