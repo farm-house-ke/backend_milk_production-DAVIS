11804afada60cc50aa90f3b55f77912cace89fe3
DAVIS <davis@kali>
2024-10-20 09:34:27 +0300

    url handling for profile view

--- user_api/urls.py
+++ user_api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("register/", UserViewSet.as_view({"post": "register"}), name="register"),
     path("login/", UserViewSet.as_view({"post": "login"}), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('upload-profile-image/',ProfileImageUploadView.as_view(),name='uploads-profile-image'),
+    path('upload-profile-image/',ProfileImageUploadView.as_view(),name='uploads-profile-image'),#url handling for profile view
 ]
