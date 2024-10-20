f9979345d7146b7e3e8d531a24d86e60acff9a4e
DAVIS <davis@kali>
2024-10-20 09:31:50 +0300

    register profile model in admin panel

--- user_api/admin.py
+++ user_api/admin.py
@@ -1,5 +1,5 @@
 """registering the models to the admin panel."""
 from django.contrib import admin
 from .models import Profile
-
+# register profile model in admin panel
 admin.site.register(Profile)
